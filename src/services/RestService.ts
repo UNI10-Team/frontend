fdc925b9c2a469898b8f7cf41c630385884f70fd
Silviu-Mihnea Cucuiet <Silviu.Cucuiet@msg.group>
2019-12-07 12:57:54 +0200

    modified restservice

--- src/services/RestService.ts
+++ src/services/RestService.ts
@@ -1,11 +1,12 @@
 export class RestService {
 
     get(path: string, headers?: Headers): Promise<any> {
-        return fetch(path, {headers});
+        return fetch(path, {headers}).then(data => data.json());
     }
 
     post(path: string, body: any, headers?: Headers): Promise<any> {
-        return fetch(path, {body, headers});
+        body = JSON.stringify(body);
+        return fetch(path, {body, headers}).then(data => data.json());
     }
 }
 
