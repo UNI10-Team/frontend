1ab6daaa0aba3b81485c52071e3593d900b95063
Silviu-Mihnea Cucuiet <Silviu.Cucuiet@msg.group>
2019-12-07 13:41:56 +0200

    add paths

---
+++ src/paths.ts
@@ -0,0 +1,5 @@
+export const localhost = 'http://localhost:8080';
+export const heroku = 'https://uni10-api.herokuapp.com';
+export const defaultURL = '';
+export const users = '';
+export const courses = '';
