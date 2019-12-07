7505d9c80afcca4c89811a955416b60190fde6e8
Amalia Bunea <50477823+amaliabunea@users.noreply.github.com>
2019-12-07 19:01:35 +0200

    attachments

--- src/components/attachment-drop/attachmentDrop.tsx
+++ src/components/attachment-drop/attachmentDrop.tsx
@@ -5,16 +5,20 @@ interface AttachmentDropProperties {
 }
 
 interface AttachmentDropState {
-
+    files: FileList,
+    error: string,
+    msg: string
 }
 
-export class AttachmentDrop extends Component{
+export class AttachmentDrop extends Component<AttachmentDropProperties, AttachmentDropState> {
+    private attachments: any = [];
 
 
-    render(){
+    render() {
         return (
             <div onDragOver={this.onDragOver} onDragLeave={this.onDragLeave}>
                 <input type="file" multiple onChange={this.onChange}/>
+                <button onClick={this.uploadFile}>Upload</button>
             </div>
         )
     }
@@ -30,20 +34,31 @@ export class AttachmentDrop extends Component{
     };
 
     onChange = (event: any) => {
-        /*
-        if (event.target.files && event.target.files.length) {
-            const files: FileList = event.target.files;
-            for (let i = 0; i < files.length; i++) {
-                const file = files.item(i);
-                console.log(file.type);
+        this.setState({
+            files: event.target.files
+        })
+    };
+
+    uploadFile = (event: any) => {
+        event.preventDefault();
+        this.setState({error: '', msg: ''});
+
+        if (!this.state.files) {
+            this.setState({error: 'Please upload files.'});
+            return;
+        }
+        const files = this.state.files;
+        for (let i = 0; i < files.length; i++) {
+            const file = files.item(i);
+            if (file != null) {
                 const reader = new FileReader();
                 reader.readAsDataURL(file);
                 reader.onload = () => {
-                    this.attachments.push({id: -1, name : file.name, file: reader.result, bugId: -1, type: file.type});
-                };
+                    this.attachments.push({id: -1, name: file.name, file: reader.result, bugId: -1, type: file.type});
+                }
             }
-            this.cd.markForCheck();
         }
-         */
-    };
+        // attachments contains all the uploaded files
+        console.log(this.attachments);
+    }
 }
--- src/paths.ts
+++ src/paths.ts
@@ -3,3 +3,4 @@ export const heroku = 'https://uni10-api.herokuapp.com';
 export const defaultURL = '';
 export const users = '';
 export const courses = '';
+export const books = '';
