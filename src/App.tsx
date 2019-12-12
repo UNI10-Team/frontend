0831d11b0e16bb2dc11d1626b575180879a48adf
Silviu-Mihnea Cucuiet <Silviu.Cucuiet@msg.group>
2019-12-12 23:56:45 +0200

    css, nls
    added variables for css and messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {AttachmentDrop} from "./components/attachment-drop/attachmentDrop";
 import {StudentCoursesComponent} from "./components/student-courses/StudentCoursesComponent";
-import {Login} from "./components/login/Login";
-import Register from "./components/register/Register";
 import './App.css';
 const App: React.FC = () => {
   return (
--- src/components/student-courses/StudentCoursesComponent.tsx
+++ src/components/student-courses/StudentCoursesComponent.tsx
@@ -2,8 +2,9 @@ import React, {Component} from "react";
 import { Filter1, Filter2, Filter3, Filter4, Home, AccountCircle, SettingsPower } from "@material-ui/icons"
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import './StudentCourses.css'
+import bundle from '../../util/nls';
 
 export interface StudentCoursesProperties {
 
@@ -14,39 +15,35 @@ export interface StudentCoursesState {
 }
 
 export class StudentCoursesComponent extends Component<StudentCoursesProperties, StudentCoursesState> {
-
-    constructor(props: StudentCoursesProperties) {
-        super(props);
-    }
     render() {
+        const messages = bundle.messages;
         return (
             <div className={"student-courses-component"}>
-
                 <div className={"grey-rectangle"}>
                     <div className={"logo"}/>
                     <div className={"main-text-1"}>
-                        Ce vei studia astazi?
+                        {messages.WHAT_WILL_YOU_STUDY_TODAY}
                     </div>
                     <div className={"main-text-2"}>
-                        Arunca o privire peste materiile disponibile.
+                        {messages.TAKE_A_LOOK_TO_COURSES}
                     </div>
                 </div>
-                <LightTooltip title={"Anul 1"} placement={"right"}>
+                <LightTooltip title={`${messages.YEAR} 1`} placement={"right"}>
                 <Button className={"button-1"}>
                     <Filter1 className={"icon"}/>
                 </Button>
                 </LightTooltip>
-                <LightTooltip title={"Anul 2"} placement={"right"}>
+                <LightTooltip title={`${messages.YEAR} 2`} placement={"right"}>
                 <Button className={"button-2"}>
                     <Filter2 className={"icon"}/>
                 </Button>
                 </LightTooltip>
-                <LightTooltip title={"Anul 3"} placement={"right"}>
+                <LightTooltip title={`${messages.YEAR} 3`} placement={"right"}>
                 <Button className={"button-3"}>
                     <Filter3 className={"icon"}/>
                 </Button>
                 </LightTooltip>
-                <LightTooltip title={"Anul 4"} placement={"right"}>
+                <LightTooltip title={`${messages.YEAR} 4`} placement={"right"}>
                 <Button className={"button-4"}>
                     <Filter4 className={"icon"}/>
                 </Button>
@@ -68,7 +65,7 @@ export class StudentCoursesComponent extends Component<StudentCoursesProperties,
 const LightTooltip = withStyles(theme => ({
     tooltip: {
         backgroundColor: theme.palette.common.black,
-        color: 'rgba(255, 233, 36, 0.69) ',
+        color: 'rgba(255, 233, 36, 0.69)',
         boxShadow: theme.shadows[1],
         fontSize: 20,
     },
