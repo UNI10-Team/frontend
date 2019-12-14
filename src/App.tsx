8165b404a495db5cc14e610d541bdb62e5499f53
Cirstea Stefan <stefancirstea98@gmail.com>
2019-12-14 15:52:19 +0200

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import {StudentCoursesComponent} from "./components/student-courses/StudentCoursesComponent";
 import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { Login } from './components/login/Login';
+import Register from './components/register/Register';
+import NotFound from './components/not-found/NotFound';
+
 const App: React.FC = () => {
   return (
-    <StudentCoursesComponent/>
+    <React.Fragment>
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/login" />
+        <Redirect to="/not-found" />
+      </Switch>
+    </React.Fragment>
   );
 };
 
