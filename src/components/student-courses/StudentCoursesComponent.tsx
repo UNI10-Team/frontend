b065558cf31a91be4c23db3cb376146d25f74a59
Amalia Bunea <50477823+amaliabunea@users.noreply.github.com>
2020-01-05 15:49:48 +0200

    updated student-courses

--- src/components/student-courses/StudentCoursesComponent.tsx
+++ src/components/student-courses/StudentCoursesComponent.tsx
@@ -1,9 +1,14 @@
 import React, {Component} from "react";
-import { Filter1, Filter2, Filter3, Filter4, Home, AccountCircle, SettingsPower } from "@material-ui/icons"
+import {Filter1, Filter2, Filter3, Filter4, Home, AccountCircle, SettingsPower} from "@material-ui/icons"
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import './StudentCourses.css'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 export interface StudentCoursesProperties {
 
@@ -18,10 +23,10 @@ export class StudentCoursesComponent extends Component<StudentCoursesProperties,
     constructor(props: StudentCoursesProperties) {
         super(props);
     }
+
     render() {
         return (
             <div className={"student-courses-component"}>
-
                 <div className={"grey-rectangle"}>
                     <div className={"logo"}/>
                     <div className={"main-text-1"}>
@@ -31,26 +36,6 @@ export class StudentCoursesComponent extends Component<StudentCoursesProperties,
                         Arunca o privire peste materiile disponibile.
                     </div>
                 </div>
-                <LightTooltip title={"Anul 1"} placement={"right"}>
-                <Button className={"button-1"}>
-                    <Filter1 className={"icon"}/>
-                </Button>
-                </LightTooltip>
-                <LightTooltip title={"Anul 2"} placement={"right"}>
-                <Button className={"button-2"}>
-                    <Filter2 className={"icon"}/>
-                </Button>
-                </LightTooltip>
-                <LightTooltip title={"Anul 3"} placement={"right"}>
-                <Button className={"button-3"}>
-                    <Filter3 className={"icon"}/>
-                </Button>
-                </LightTooltip>
-                <LightTooltip title={"Anul 4"} placement={"right"}>
-                <Button className={"button-4"}>
-                    <Filter4 className={"icon"}/>
-                </Button>
-                </LightTooltip>
                 <Button className={"button-turnoff"}>
                     <SettingsPower className={"icon"}/>
                 </Button>
@@ -60,6 +45,74 @@ export class StudentCoursesComponent extends Component<StudentCoursesProperties,
                 <Button className={"button-home"}>
                     <Home className={"icon"}/>
                 </Button>
+                <LightTooltip title={"Anul 1"} placement={"right"}>
+                    <ExpansionPanel className={"panel-1"}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                            <Filter1 className={"icon"}/>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                FP
+                            </Button>
+                        </ExpansionPanelDetails>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                ASC
+                            </Button>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </LightTooltip>
+                <LightTooltip title={"Anul 2"} placement={"right"}>
+                    <ExpansionPanel className={"panel"}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                            <Filter2 className={"icon"}/>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                MPP
+                            </Button>
+                        </ExpansionPanelDetails>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                MAP
+                            </Button>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </LightTooltip>
+                <LightTooltip title={"Anul 3"} placement={"right"}>
+                    <ExpansionPanel className={"panel"}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                            <Filter3 className={"icon"}/>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                MPP
+                            </Button>
+                        </ExpansionPanelDetails>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                MAP
+                            </Button>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </LightTooltip>
+                <LightTooltip title={"Anul 4"} placement={"right"}>
+                    <ExpansionPanel className={"panel"}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                            <Filter4 className={"icon"}/>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                MPP
+                            </Button>
+                        </ExpansionPanelDetails>
+                        <ExpansionPanelDetails>
+                            <Button className={"button"}>
+                                MAP
+                            </Button>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </LightTooltip>
             </div>
         );
     }
