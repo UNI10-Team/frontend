d97719d7765740fa015d7934f12472979eb23ba3
uni10-team <uni10@scs.ubbcluj.ro>
2020-01-13 10:16:34 +0200

    Teacher Courses

---
+++ src/components/courses-viewer/CoursesViewer.tsx
@@ -0,0 +1,74 @@
+import React, {Component} from "react";
+import Button from '@material-ui/core/Button';
+import './CoursesViewer.css'
+import bundle from '../../util/nls';
+import DesktopMacIcon from '@material-ui/icons/DesktopMac';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Attachment from "../../interfaces/attachment";
+import Course from "../../interfaces/course";
+import {RightMenuComponent} from "../right-menu/RightMenuComponent";
+import {LeftMenuComponent} from "../left-menu/LeftMenuComponent";
+
+export interface CoursesViewerProperties {
+}
+
+export interface CoursesViewerState {
+    course?: Course,
+    attachments: Attachment[]
+}
+
+export class CoursesViewer extends Component<CoursesViewerProperties, CoursesViewerState> {
+
+    constructor(props: CoursesViewerProperties) {
+        super(props);
+        this.state = {
+            attachments: []
+        }
+    }
+
+    render() {
+        const messages = bundle.messages;
+        const {attachments = []} = this.state;
+        return (
+            <div className={"courses-viewer-component"}>
+                <div className={"grey-rectangle-courses"}>
+                    <div className={"left"}>
+                        <Button> <ArrowBackIosIcon className={"left-icon"}/> </Button>
+                    </div>
+                    <div className={"right"}>
+                        <DesktopMacIcon className={"desktop-icon"}/>
+                        <div>{messages.COURSES}</div>
+                    </div>
+                    <ButtonGroup className={"button-group"} >
+                        {/*DO NOT DELETE!!!!!*/}
+                        {/*{attachments.map((att: Attachment) =>*/}
+                        {/*<Button>`#${att.id}`</Button>)}*/}
+
+                        <Button className={"course-button"} >#1</Button>
+                        <Button className={"course-button"}>#2</Button>
+                        <Button className={"course-button"}>#3</Button>
+                        <Button className={"course-button"}>#4</Button>
+                        <Button className={"course-button"}>#5</Button>
+                        <Button className={"course-button"}>#6</Button>
+                        <Button className={"course-button"}>#7</Button>
+                        <Button className={"course-button"}>#8</Button>
+                        <Button className={"course-button"}>#9</Button>
+                        <Button className={"course-button"}>#10</Button>
+                        <Button className={"course-button"}>#11</Button>
+                        <Button className={"course-button"}> #12</Button>
+                        <Button className={"course-button"}>#13</Button>
+                        <Button className={"course-button"}>#14</Button>
+
+
+                    </ButtonGroup>
+
+                </div>
+                <RightMenuComponent role={"student"}/>
+                <LeftMenuComponent/>
+            </div>
+        );
+    }
+}
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -5,32 +5,31 @@ import * as serviceWorker from "./serviceWorker";
 import { Router, Switch, Route, Redirect } from "react-router-dom";
 import { Login } from "./components/login/Login";
 import Register from "./components/register/Register";
-import { StudentHomePage } from "./components/home/student-home/StudentHomePage";
-import { TeacherHomePage } from "./components/home/teacher-home/TeacherHomePage";
-import { StudentProfile } from "./components/profile/student-profile/StudentProfile";
-import { TeacherProfile } from "./components/profile/teacher-profile/TeacherProfile";
-import { StudentCourses } from "./components/student-courses/StudentCourses";
-import { StudentCourse } from "./components/student-course/StudentCourse";
-import { TeacherCourses } from "./components/teacher-courses/TeacherCourses";
+import {StudentHomePage} from "./components/home/student-home/StudentHomePage";
+import {TeacherHomePage} from "./components/home/teacher-home/TeacherHomePage";
+import {StudentProfile} from "./components/profile/student-profile/StudentProfile";
+import {TeacherProfile} from "./components/profile/teacher-profile/TeacherProfile";
+import {StudentCourses} from "./components/student-courses/StudentCourses";
+import {TeacherCourses} from "./components/teacher-courses/TeacherCourses";
+import {CoursesViewer} from "./components/courses-viewer/CoursesViewer";
 import history from "./history";
 
 ReactDOM.render(
-  <Router history={history}>
-    <Switch>
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
-      <Route path="/student/home" component={StudentHomePage} />
-      <Route exact path="/teacher/home" component={TeacherHomePage} />
-      <Route path="/student/profile" component={StudentProfile} />
-      <Route exact path="/teacher/profile" component={TeacherProfile} />
-      <Route exact path="/student/courses" component={StudentCourses} />
-      <Route exact path="/student/course" component={StudentCourse} />
-      <Route exact path="/teacher/courses" component={TeacherCourses} />
-      <Redirect from="/" to="/login" />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+    <Router history={history}>
+        <Switch>
+            <Route path="/register" component={Register}/>
+            <Route path="/login" component={Login} />
+            <Route path="/student/home" component={StudentHomePage}/>
+            <Route exact path="/teacher/home" component={TeacherHomePage}/>
+            <Route path="/student/profile" component={StudentProfile}/>
+            <Route exact path="/teacher/profile" component={TeacherProfile}/>
+            <Route exact path="/student/courses" component={StudentCourses}/>
+            <Route exact path="/teacher/courses" component={TeacherCourses}/>
+            <Route extract path="/courses/viewer" component={CoursesViewer}/>
+            <Redirect from="/" to="/login"/>
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
