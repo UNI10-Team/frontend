2030a6be8185ddeda350ff81a827b80968c1ced2
Valentina <57238808+CheleValentina@users.noreply.github.com>
2020-01-13 08:44:51 +0200

    Student Course Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import {Router, Switch, Route, Redirect} from "react-router-dom"
-import {Login} from "./components/login/Login";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+import { Login } from "./components/login/Login";
 import Register from "./components/register/Register";
-import {StudentHomePage} from "./components/home/student-home/StudentHomePage";
-import {TeacherHomePage} from "./components/home/teacher-home/TeacherHomePage";
-import {StudentProfile} from "./components/profile/student-profile/StudentProfile";
-import {TeacherProfile} from "./components/profile/teacher-profile/TeacherProfile";
-import {StudentCourses} from "./components/student-courses/StudentCourses";
-import {TeacherCourses} from "./components/teacher-courses/TeacherCourses";
+import { StudentHomePage } from "./components/home/student-home/StudentHomePage";
+import { TeacherHomePage } from "./components/home/teacher-home/TeacherHomePage";
+import { StudentProfile } from "./components/profile/student-profile/StudentProfile";
+import { TeacherProfile } from "./components/profile/teacher-profile/TeacherProfile";
+import { StudentCourses } from "./components/student-courses/StudentCourses";
+import { StudentCourse } from "./components/student-course/StudentCourse";
+import { TeacherCourses } from "./components/teacher-courses/TeacherCourses";
 import history from "./history";
 
 ReactDOM.render(
-    <Router history={history}>
-        <Switch>
-            <Route path="/register" component={Register}/>
-            <Route path="/login" component={Login} />
-            <Route path="/student/home" component={StudentHomePage}/>
-            <Route exact path="/teacher/home" component={TeacherHomePage}/>
-            <Route path="/student/profile" component={StudentProfile}/>
-            <Route exact path="/teacher/profile" component={TeacherProfile}/>
-            <Route exact path="/student/courses" component={StudentCourses}/>
-            <Route exact path="/teacher/courses" component={TeacherCourses}/>
-            <Redirect from="/" to="/login"/>
-        </Switch>
-    </Router>,
-    document.getElementById('root'));
+  <Router history={history}>
+    <Switch>
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
+      <Route path="/student/home" component={StudentHomePage} />
+      <Route exact path="/teacher/home" component={TeacherHomePage} />
+      <Route path="/student/profile" component={StudentProfile} />
+      <Route exact path="/teacher/profile" component={TeacherProfile} />
+      <Route exact path="/student/courses" component={StudentCourses} />
+      <Route exact path="/student/course" component={StudentCourse} />
+      <Route exact path="/teacher/courses" component={TeacherCourses} />
+      <Redirect from="/" to="/login" />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
